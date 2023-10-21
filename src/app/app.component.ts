58b16888345f3d87d13224326f1c17e184ea90f1
ajmris <szymektarasiewicz@onet.pl>
2023-10-21 17:20:56 +0200

    todo list with LocalStorge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  newTask: string='';
+  tasksList: Array<string>=[];
+  tasksDone: Array<string>=[];
+  // W konstruktorze lub inicjalizacji komponentu
+  constructor() {
+    const storedTasks = localStorage.getItem('tasksList');
+    if (storedTasks) {
+      this.tasksList = JSON.parse(storedTasks);
+    }
+  }
+  add(){
+    this.tasksList.push(this.newTask);
+    this.newTask='';
+    localStorage.setItem('tasksList', JSON.stringify(this.tasksList));
+  }
+  remove(task: string){
+    this.tasksList=this.tasksList.filter(e=>e !== task);
+  }
+  done(task: string){
+    this.tasksDone.push(task);
+    this.remove(task);
+    console.log(this.tasksList);
+  }
 }
